1010b2c47b6edbbb2a89b6f2c50d23d97d14865d
Ksenia Timokhina <kseenina@gmail.com>
2026-05-13 18:29:40 +0300

    Task 1.base-concepts completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let arr = [];
+    let d = b ** 2 - 4 * a * c;
+    if (d < 0) {
+        return arr;
+    } else if (d === 0) {
+        arr[0] = -b / (2 * a)
+    } else if (d > 0) {
+        arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+        arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+    }
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    let monthPercent = percent / 100 / 12;
+    let debt = amount - contribution;
+    let monthPayment = debt * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
+    let totalPayment = monthPayment * countMonths;
+    return Number(totalPayment.toFixed(2));
 }
